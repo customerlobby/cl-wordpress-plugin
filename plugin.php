a32b1848c49ec5f5f1cf958e6f76adfc96c4843e
spiritbear <indap.shiv@gmail.com>
2012-11-15 12:13:46 -0800

    CSS Chnages

--- plugin.php
+++ plugin.php
@@ -110,7 +110,7 @@ if (!file_exists(CL_PATH . "/cached") || !is_writable(CL_PATH . "/cached")) {
         <span class='item'><span class='photo'>&nbsp;</span>
         <span class='fn'>{$data['title']}</span></span>
         <p class='hreview-count'><span class='count'>{$data['count']}</span> " . __('customer reviews') . "</p>
-        <p class='hreview-average left'>" . __('average rating:') . "<span class='average rating'>{$data['average']}</span></p>
+        <p class='hreview-average left'>" . __('Average rating:') . "<span class='average rating'>{$data['average']}</span></p>
         <div class='rating rating-{$data['average']} left stars-align'>&nbsp;</div>
         <div class='clear'></div>
     </div>
